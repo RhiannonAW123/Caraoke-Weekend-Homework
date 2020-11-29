4fe790df28124208b5fd83f175c86d9903a64470
RhiannonAW123 <rhiannonaimeewilson@gmail.com>
2020-11-29 17:50:03 +0000

    Initial song tests passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -11,3 +11,13 @@ class TestSong(unittest.TestCase):
         self.assertEqual("Juicy", self.song_1.title)
         self.assertEqual("Club Tropicana", self.song_2.title)
         self.assertEqual("Thunderstruck", self.song_3.title)
+
+    def test_song_has_artist(self):
+        self.assertEqual("The Notorious B.I.G", self.song_1.artist)
+        self.assertEqual("Wham", self.song_2.artist)
+        self.assertEqual("ACDC", self.song_3.artist)
+
+    def test_song_has_genre(self):
+        self.assertEqual("Hip Hop", self.song_1.genre)
+        self.assertEqual("Classics", self.song_2.genre)
+        self.assertEqual("Rock", self.song_3.genre)
